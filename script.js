7408796f9a93f2e16dd007c3b08bbc0f4b895894
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-15 15:48:12 +0200

    versione con le 10 mail tutte uguali, da correggere

---
+++ script.js
@@ -0,0 +1,26 @@
+/* SELEZIONIAMO LE VARIABILI API*/
+const endPointApi = "https://flynn.boolean.careers/exercises/api/random/mail"
+
+console.log(endPointApi);
+
+
+/* OUTPUT */
+const stamp = document.querySelector(".boxList")
+
+/* Chiamata AJAX */
+axios.get(endPointApi)
+       .then(rispApi => {
+        /* Codice da eseguite in caso di successo */
+        const mailRandom = rispApi.data.response;
+        console.log(mailRandom)
+
+        for (i = 0; i < 10; i++) {
+            stamp.innerHTML += `<li> ${mailRandom}</li>`        
+        }
+
+
+       })
+       .catch(error => {
+        /* Codice da esewguire in caso di errore */
+        console.error(error)
+       })
